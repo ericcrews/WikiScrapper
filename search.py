388889e11e17293961cd6ec72f7d666c16de59c7
ericcrews <40183090+ericcrews@users.noreply.github.com>
2018-06-11 18:18:21 -0400

    Update search.py
    Added helper function for gathering articles that link to a given page

--- search.py
+++ search.py
@@ -1,5 +1,23 @@
 # Takes two args from the user
 from sys import argv
+from bs4 import BeautifulSoup
+import requests
+
+
+def what_links_here(article):
+    """
+    Returns a list of all Wikipedia articles that this article links to.
+    """
+    articles = []
+    html_page = requests.get(
+        f'https://en.wikipedia.org/w/index.php?title=Special:WhatLinksHere/'
+        f'{article}&namespace=0&limit=500&hideredirs=1&hidetrans=1')
+    soup = BeautifulSoup(html_page.text, "lxml")
+    for span in soup.findAll('span', {'class': 'mw-whatlinkshere-tools'}):
+        link = span.contents[1]['href']
+        index = link.find('target=')
+        articles.append(link[index + len('target='):])
+
 
 scriptName, firstArg, secondArg = argv
 rootPage = "https://en.wikipedia.org/wiki/"
@@ -7,3 +25,4 @@ startPage = rootPage + firstArg.replace(" ","_")
 endPage = rootPage + secondArg.replace(" ","_")
 print("Executing {0}! Your starting page is {1} and your end location is {2}".format(scriptName,startPage,endPage))
 
+what_links_here(rootPage)
